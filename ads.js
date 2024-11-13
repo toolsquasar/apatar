ddf99f38f5660c2e9470f7c6b812f796c8b1f042
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2024-11-13 13:33:55 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '97127cc57dc93cfc34b57f8d64d1d5c6',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/97127cc57dc93cfc34b57f8d64d1d5c6/invoke.js"></scr' + 'ipt>');
