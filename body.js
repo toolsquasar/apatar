3d37c766ec3261931e84619b415a9a3317040f14
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2024-11-13 11:26:26 +0700

    Add files via upload

---
+++ body.js
@@ -0,0 +1,11 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+
+//Paste Iklan Disini
+
+document.write('<script type="text/javascript">document.write(atob("CglhdE9wdGlvbnMgPSB7CgkJJ2tleScgOiAnOTcxMjdjYzU3ZGM5M2NmYzM0YjU3ZjhkNjRkMWQ1YzYnLAoJCSdmb3JtYXQnIDogJ2lmcmFtZScsCgkJJ2hlaWdodCcgOiA5MCwKCQknd2lkdGgnIDogNzI4LAoJCSdwYXJhbXMnIDoge30KCX07Cglkb2N1bWVudC53cml0ZSgnPHNjcicgKyAnaXB0IHR5cGU9InRleHQvamF2YXNjcmlwdCIgc3JjPSIvL3d3dy5oaWdocGVyZm9ybWFuY2Vmb3JtYXQuY29tLzk3MTI3Y2M1N2RjOTNjZmMzNGI1N2Y4ZDY0ZDFkNWM2L2ludm9rZS5qcyI+PC9zY3InICsgJ2lwdD4nKTsK"))</script>');
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
