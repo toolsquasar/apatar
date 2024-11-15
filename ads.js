c031e1634309afe8ad57090c7026b578f48e1b8d
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2024-11-15 13:46:08 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,9 +1,9 @@
 
 	atOptions = {
-		'key' : '97127cc57dc93cfc34b57f8d64d1d5c6',
+		'key' : 'ad5109b9b5c7e2217f65e849fda5d903',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/97127cc57dc93cfc34b57f8d64d1d5c6/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/ad5109b9b5c7e2217f65e849fda5d903/invoke.js"></scr' + 'ipt>');
