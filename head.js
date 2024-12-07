1e7e8bcefb3db5136783fb756c3f8e71ea22f314
toolsquasar <126128791+toolsquasar@users.noreply.github.com>
2024-12-07 22:24:19 +0700

    Update head.js

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
-
-
 ///// Arsae /////
 var ars = 'https://blog.uof.edu.pl';
 if(['.edu.', '.google.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
